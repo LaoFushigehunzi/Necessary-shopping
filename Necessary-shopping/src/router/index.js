807ce8537c8a38f50b8aae999bf73111a4332659
LaoFushigehunzi <z1846355562@163.com>
2023-01-05 18:03:51 +0800

    '后台接口初步搭建完成'

---
+++ Necessary-shopping/src/router/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mysql = require("promise-mysql");
+const router = express.Router();
+let pool;
+(async () => {
+  pool = await mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "0000",
+    database: "我们的小小商",
+  });
+})();
+router.get("/api/one", async (req, res, next) => {
+  let result = await pool.query("select * from arr_one");
+  res.json({
+    code: 200,
+    message: "请求成功",
+    data: result,
+  });
+});
+router.get("/api/two", async (req, res, next) => {
+  let result = await pool.query("select * from arr_two");
+  res.json({
+    code: 200,
+    message: "请求成功",
+    data: result,
+  });
+});
+router.get("/api/three", async (req, res, next) => {
+  let result = await pool.query("select * from arr_three");
+  res.json({
+    code: 200,
+    message: "请求成功",
+    data: result,
+  });
+});
+router.get("/api/goods", async (req, res, next) => {
+  let result = await pool.query("select * from goods");
+  res.json({
+    code: 200,
+    message: "请求成功",
+    data: result,
+  });
+});
+router.use(function (err, req, res, next) {
+  res.json({
+    code: 400,
+    message: err.message,
+  });
+});
+module.exports = router;
