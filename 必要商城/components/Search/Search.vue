981b1d43182474b218e5c737f8c5b2ecb8e6c852
LaoFushigehunzi <z1846355562@163.com>
2023-01-06 16:33:59 +0800

    '更新了数据接口，和项目请求'

---
+++ 必要商城/components/Search/Search.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="root">
+		搜索
+		<input type="text" placeholder="请输入要搜索的商品">
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "Search",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	.root {
+		width: 90%;
+		// background-color: #F4F4F4;
+		display: flex;
+		background-color: red;
+		margin: auto;
+		height: 80rpx;
+		border-radius: 40rpx;
+		padding: 0 40rpx;
+
+		input {}
+	}
+</style>
--- 必要商城/pages/index/index.vue
+++ 必要商城/pages/index/index.vue
@@ -1,15 +1,30 @@
 <template>
 	<view>
-		首页
+		<Search></Search>
 	</view>
 </template>
 
 <script>
+	import {
+		axiosGet
+	} from '@/common/js/http.js'
+	import Search from '@/components/Search/Search.vue'
 	export default {
 		data() {
 			return {
-
+				data: []
 			};
+		},
+		created() {
+			this.getData()
+		},
+		methods: {
+			async getData() {
+				let result = await axiosGet('/api/one');
+				if (result.code == 200) {
+					this.data = result.data
+				}
+			}
 		}
 	}
 </script>
