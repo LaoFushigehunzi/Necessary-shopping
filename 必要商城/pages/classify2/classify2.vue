dfe984c81f79ef8c209720b60305168490baa645
lisalike <3498322774@qq.com>
2023-01-08 12:16:15 +0800

    lww试提交

--- 必要商城/pages/classify2/classify2.vue
+++ 必要商城/pages/classify2/classify2.vue
@@ -1,37 +1,134 @@
 <template>
 	<view>
-		<uni-nav-bar  title="男装" left-icon="back" statusBar="true" fixed="true" @clickLeft="comeback"></uni-nav-bar>
-		
-		
+		<uni-nav-bar   title="" left-icon="back" statusBar="true" fixed="true" @clickLeft="comeback"></uni-nav-bar>
+			<!-- <view>
+				<scroll-view class="scroll-view_H" v-for="item in three_data" :key="item.three_id" scroll-x="true" @scroll="scroll" scroll-left="20">
+					<view id="demo1" class="scroll-view-item_H uni-bg-red">{{item.name}}</view>
+				</scroll-view>
+			</view> -->
+			<view>
+				<scroll-view class="scroll-view_H" scroll-x="true" @scroll="scroll" scroll-left="120">
+					<view id="demo1" class="scroll-view-item_H uni-bg-red">风扇</view>
+					<view id="demo2" class="scroll-view-item_H uni-bg-green">清洁吸尘</view>
+					<view id="demo3" class="scroll-view-item_H uni-bg-blue">加湿净化</view>
+					<view id="demo5" class="scroll-view-item_H uni-bg-red">风扇</view>
+					<view id="demo6" class="scroll-view-item_H uni-bg-green">清洁吸尘</view>
+					<view id="demo7" class="scroll-view-item_H uni-bg-blue">加湿净化</view>
+					<view id="demo8" class="scroll-view-item_H uni-bg-red">风扇</view>
+					<view id="demo9" class="scroll-view-item_H uni-bg-green">清洁吸尘</view>
+					<view id="demo10" class="scroll-view-item_H uni-bg-blue">加湿净化</view>
+				</scroll-view>
+			</view>
 	</view>
 </template>
 
 <script>
 	import {axiosGet} from'@/utils/http.js'
 	export default {
+		//响应性属性
 		data() {
 			return {
-				onename:[]
+				one_id:334,
+				three_id:561,
+				three:'',
+				twoname:[],
+				threename:[],
+				three_data:[],
+				scrollTop: 0,
+				old: {
+					scrollTop: 0
+				}
+				
 			}
 		},
+		created(){
+			this.getTwoname(),
+			this.getThreename()
+		},
+		onLoad() {},
 		methods: {
 			comeback() {
 				uni.switchTab({
 					url: '../index/index'
 				})
+			},
+			async getTwoname(){
+				let result=await axiosGet("/api/two");
+				if(+result.code===200){
+					this.twoname=result.data;
+					console.log(this.twoname)
+				}
+			},
+			async getThreename(){
+				let one_id =this.one_id;
+				let two_id=this.two_id;
+				let three_id =this.three_id;
+				let result1=await axiosGet("/api/one");
+				let result3=await axiosGet("/api/three");
+				let one_item=result1.data.find(item=>item.one_id==one_id)
+				let three_item=result3.data.find(item=>item.three_id==three_id)
+				let three_data=result3.data.filter((item)=>{return item.two_id==three_item.two_id})
+				
+				this.three_data=three_data
+				console.log(three_item)
+				console.log(three_data)
+				// if(+result.code===200){
+				// 	console.log(result)
+				// 	this.threename=result.data;
+				// 	console.log(this.threename)
+				// }
+			},
+			upper: function(e) {
+				console.log(e)
+			},
+			lower: function(e) {
+				console.log(e)
+			},
+			scroll: function(e) {
+				console.log(e)
+				this.old.scrollTop = e.detail.scrollTop
+			},
+			goTop: function(e) {
+				// 解决view层不同步的问题
+				this.scrollTop = this.old.scrollTop
+				this.$nextTick(function() {
+					this.scrollTop = 0
+				});
+				uni.showToast({
+					icon: "none",
+					title: "纵向滚动 scrollTop 值已被修改为 0"
+				})
 			}
+			
 		},
-		async created(){
-			let result=await axiosGet("/api/one");
-			console.log(result)
-			if(+result.code===200){
-				this.onename=result.data;
-			}
-		}
+		
 		 
 		
 	}
 </script>
 
 <style>
+	.scroll-Y {
+		height: 20rpx;
+	}
+	.scroll-view_H {
+		white-space: nowrap;
+		width: 100%;
+		display: flex;
+	}
+	.scroll-view-item {
+		height: 300rpx;
+		width: 25%;
+		line-height: 300rpx;
+		text-align: center;
+		font-size: 36rpx;
+	}
+	.scroll-view-item_H {
+		display: inline-block;
+		width: 100%;
+		height: 20rpx;
+		line-height: 20rpx;
+		text-align: center;
+		font-size: 14rpx;
+	}
 </style>
--- 必要商城/pages/order_all/order_all.vue
+++ 必要商城/pages/order_all/order_all.vue
@@ -1,18 +1,42 @@
 <template>
 	<view>
 		<uni-nav-bar title="全部订单" left-icon="back" statusBar="true" fixed="true" @clickLeft="comeback"></uni-nav-bar>
-		全部订单
+		
+		<view class="tabs">
+		    <scroll-view scroll-x class="scroll-h" >
+		    <block v-for="(tab,index) in tabBars" :key="tab.id">
+		    <view class="uni-tab-item" :class="{'uni-tab-item-title-active' :tabIndex==index}" @tap="tabtap(index)">
+		    <!--1 {{tab.name}} -->
+		    <!--2 <text class="uni-tab-item-title" :class="tabIndex==index ? 'uni-tab-item-title-active' : ''">{{tab.name}}</text> -->
+		    </view>
+		    </block>            
+		    </scroll-view>
+		</view>
+
 	</view>
 </template>
 
-<script>
+<script >
 	export default {
 		data() {
 			return {
-
+				tabIndex:0,
+				    tabBars:[
+				        { name:"全部",id:"qb"},
+				        { name:"待付款",id:"dfk"},
+				        { name:"待成团",id:"dct"},
+				        { name:"生产/待收货",id:"sc/dsh"},
+				        { name:"待评价",id:"dpj"},
+				    ]
 			}
 		},
+		onLoad(){
+			
+		},
 		methods: {
+			tabtap(index){
+			        this.tabIndex=index;
+			    },
 			comeback() {
 				uni.switchTab({
 					url: '../index/index'
@@ -22,6 +46,56 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.tabs {
+    flex: 1;
+    flex-direction: column;
+    overflow: hidden;
+    background-color: #ffffff;
+    /* #ifdef MP-ALIPAY || MP-BAIDU */
+    height: 100vh;
+    /* #endif */
+}
 
+.scroll-h {
+    width: 750upx;
+    height: 80upx;
+    flex-direction: row;
+    /* #ifndef APP-PLUS */
+    white-space: nowrap;
+    /* #endif */
+    /* flex-wrap: nowrap; */
+    /* border-color: #cccccc;
+    border-bottom-style: solid;
+    border-bottom-width: 1px; */
+}
+
+.line-h {
+    height: 1upx;
+    background-color: #cccccc;
+}
+
+.uni-tab-item {
+    /* #ifndef APP-PLUS */
+    display: inline-block;
+    /* #endif */
+    flex-wrap: nowrap;
+    padding-left: 34upx;
+    padding-right: 34upx;
+}
+
+.uni-tab-item-title {
+    color: #555;
+    font-size: 30upx;
+    height: 80upx;
+    line-height: 80upx;
+    flex-wrap: nowrap;
+    /* #ifndef APP-PLUS */
+    white-space: nowrap;
+    /* #endif */
+}
+
+.uni-tab-item-title-active {
+    color: #007AFF;
+}
 </style>
